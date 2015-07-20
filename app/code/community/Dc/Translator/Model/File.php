e27e8e2eed95d65e605494ce0d17ef222f7f5dc7
Damián Culotta <damianculotta@gmail.com>
2015-07-19 21:13:37 -0300

    Fixed code style.

--- app/code/community/Dc/Translator/Model/File.php
+++ app/code/community/Dc/Translator/Model/File.php
@@ -29,66 +29,82 @@ class Dc_Translator_Model_File extends Mage_Core_Model_Abstract
      */
     public function saveByModule($data)
     {
-        $_model = Mage::getModel('translator/file')->getCollection()
+        $model = Mage::getModel('translator/file')->getCollection()
                     ->addFieldToFilter('package_module', array('eq' => $data['package_module']));
-        if ($_model->getSize() > 0) {
-            $_package = Mage::getModel('translator/file')->load($_model->getFirstItem()->getFileId());
-            $_package->setFileName($data['file_name']);
-            $_package->save();
+        if ($model->getSize() > 0) {
+            $package = Mage::getModel('translator/file')->load($model->getFirstItem()->getFileId());
+            $package->setFileName($data['file_name']);
+            $package->save();
         } else {
             Mage::getModel('translator/file')->setData($data)->save();
         }
         return true;
     }
-    
+
+    /**
+     * Get the list of existing files inside a package.
+     *
+     * @param $locale
+     * @return array
+     */
     public function getExistingFiles($locale)
     {
-        $_files = array();
-        $_list = array();
+        $files = array();
+        $list = array();
         $path = Mage::getBaseDir() . DS . 'app' . DS . 'locale' . DS . $locale;
         if (file_exists($path)) {
             $directory = dir($path);
             while ($file = $directory->read()) {
                 if (strrpos($file, '.csv')) {
-                    $_list[strtolower($file)] = $file;
+                    $list[strtolower($file)] = $file;
                 }
             }
-            ksort($_list, SORT_STRING);
-            foreach ($_list as $_item) {
-                $_files[$_item] = array('value' => $_item, 'label' => $_item);
+            ksort($list, SORT_STRING);
+            foreach ($list as $item) {
+                $files[$item] = array('value' => $item, 'label' => $item);
             }
             $directory->close();
         }
-        return $_files;
+        return $files;
     }
-    
+
+    /**
+     * Creates a zip file on the fly.
+     *
+     * @param $locale
+     * @param array $files
+     */
     public function downloadZip($locale, array $files)
     {
-        $_filename = $locale . '-package.zip';
-        $_zip = Mage::helper('translator/zip');
-        $_zip->init($_filename);
-        $_date = new Zend_Date(Mage::app()->getLocale()->date(now()), Zend_Date::ISO_8601);
-        $_data = sprintf('File downloaded from %s by %s on %s',
+        $filename = $locale . '-package.zip';
+        $zip = Mage::helper('translator/zip');
+        $zip->init($filename);
+        $date = new Zend_Date(Mage::app()->getLocale()->date(now()), Zend_Date::ISO_8601);
+        $data = sprintf('File downloaded from %s by %s on %s',
                             Mage::getBaseUrl(),
                             Mage::getSingleton('admin/session')->getUser()->getFirstname() . " " . Mage::getSingleton('admin/session')->getUser()->getLastname(),
-                            Mage::helper('core')->formatDate($_date, 'medium', true)
+                            Mage::helper('core')->formatDate($date, 'medium', true)
                         );
-        $_zip->add_file('README.txt', $_data);
-        $_path = Mage::getBaseDir(). DS . 'app' . DS . 'locale' . DS . $locale;
+        $zip->add_file('README.txt', $data);
+        $path = Mage::getBaseDir(). DS . 'app' . DS . 'locale' . DS . $locale;
         foreach ($files as $file) {
-            $_zip->add_file_from_path($file, $_path . DS . $file);
+            $zip->add_file_from_path($file, $path . DS . $file);
         }
-        $_zip->finish();
+        $zip->finish();
     }
-    
-    private function _checkFolderExists($path)
+
+    /**
+     * @param $path
+     * @return bool
+     */
+    private function checkFolderExists($path)
     {
         try {
-            $ioProxy = new Varien_Io_File();
-            $ioProxy->setAllowCreateFolders(true);
-            $ioProxy->open(array($path));
-            $ioProxy->close();
-            unset($ioProxy);
+            $io_proxy = new Varien_Io_File();
+            $io_proxy->setAllowCreateFolders(true);
+            $io_proxy->open(array($path));
+            $io_proxy->close();
+            unset($io_proxy);
             return true;
         } catch (Exception $e) {
             return false;
@@ -97,27 +113,37 @@ class Dc_Translator_Model_File extends Mage_Core_Model_Abstract
 
     /**
      * Used to process Uploadify request.
-     */    
+     *
+     * @param $locale
+     * @return bool
+     * @throws Mage_Core_Exception
+     */
     public function uploadPackageFile($locale)
     {
-        $_file_name = false;
+        $file_name = false;
         if (isset($_FILES['package_file']['name'])) {
             $path = $this->_getPackageFilesPath($locale);
-            $_folder_exists = $this->_checkFolderExists($path);
-            if ($_folder_exists) {
+            $folder_exists = $this->checkFolderExists($path);
+            if ($folder_exists) {
                 $uploader = new Varien_File_Uploader('package_file');
                 $uploader->setAllowedExtensions(array('csv'));
                 $uploader->setAllowRenameFiles(false);
                 $uploader->setFilesDispersion(false);
                 $uploader->save($path, $uploader->getUploadedFileName());
-                $_file_name = $uploader->getUploadedFileName();
+                $file_name = $uploader->getUploadedFileName();
             } else {
                 throw new Mage_Core_Exception(Mage::helper('translator')->__("Image can't be uploaded"));
             }
         }
-        return $_file_name;
+        return $file_name;
     }
-    
+
+    /**
+     * Returns package path.
+     *
+     * @param $locale
+     * @return string
+     */
     private function _getPackageFilesPath($locale)
     {
         return Mage::getBaseDir() . DS . 'app' . DS . 'locale' . DS . $locale . DS;
